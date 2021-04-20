fcbc4d59c674c589af11e5775db4dbf6eab29bc1
nicezic0 <nicezic@naver.com>
2021-04-20 13:27:08 +0900

    가나쉬를 새로 띄어서 컨트랙트 주소가 바뀌었음.

--- periphery/migrations/2_deploy_contracts.js
+++ periphery/migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ const WETH = artifacts.require('WETH.sol');
 module.exports = async function (deployer, _network, addresses) {
   let weth;
   // yhpark. core에서 배포한 UNISWAPV2FACTORY 어드레스. 트러플 결과창에서 확인 가능
-  const FACTORY_ADDRESS = '0x9Ede61FCAA0Bd83C8d7b7Ff3563D5EF6123B56f3';
+  const FACTORY_ADDRESS = '0xD9E5bcA4A89eE5c878B48909507f1171C30a90f2';
 
   if(_network === 'mainnet') {
     weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
